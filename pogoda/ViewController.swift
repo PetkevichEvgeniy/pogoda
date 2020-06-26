7084299546d5154025b3b3c486f21fae2cf142dc
PetkevichEvgeniy <inst.belsquare@gmail.com>
2020-06-26 10:57:04 +0300

    Initial Commit

---
+++ pogoda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pogoda
+//
+//  Created by Petkevich Evgeniy on 6/26/20.
+//  Copyright © 2020 Petkevich Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
